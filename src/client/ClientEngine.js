a57591a9bda78cfe5bc0116c256b188353059ac4
k@tЯRa <ya_katyara@mail.ru>
2021-07-11 14:54:39 +0300

    Homework-2 Game classes and new map

---
+++ src/client/ClientEngine.js
@@ -0,0 +1,71 @@
+import EventSourceMixin from '../common/EventSourceMixin';
+
+class ClientEngine {
+  constructor(canvas) {
+    Object.assign(this, {
+      canvas,
+      ctx: null,
+      imageLoaders: [],
+      images: {},
+      sprites: {},
+    });
+
+    this.ctx = canvas.getContext('2d');
+    this.loop = this.loop.bind(this);
+  }
+
+  start() {
+    this.loop();
+  }
+
+  loop(timestamp) {
+    const { ctx, canvas } = this;
+    ctx.fillStyle = 'black';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    this.trigger('render', timestamp);
+    this.initNextFrame();
+  }
+
+  initNextFrame() {
+    window.requestAnimationFrame(this.loop);
+  }
+
+  loadSprites(spritesGroup) {
+    this.imageLoaders = [];
+
+    Object.entries(spritesGroup).forEach(([groupName, group]) => {
+      this.sprites[groupName] = group;
+
+      Object.values(group).forEach((sprite) => {
+        const { img } = sprite;
+        if (!this.images[img]) {
+          this.imageLoaders.push(this.loadImage(img));
+        }
+      });
+    });
+
+    return Promise.all(this.imageLoaders);
+  }
+
+  loadImage(url) {
+    return new Promise((resolve) => {
+      const i = new Image();
+      this.images[url] = i;
+      i.onload = () => resolve(i);
+      i.src = url;
+    });
+  }
+  // eslint-disable-next-line
+  renderSpriteFrame({ sprite, frame, x, y, w, h }) {
+    const spriteCfg = this.sprites[sprite[0]][sprite[1]];
+    const [fx, fy, fw, fh] = spriteCfg.frames[frame];
+    const img = this.images[spriteCfg.img];
+
+    this.ctx.drawImage(img, fx, fy, fw, fh, x, y, w, h);
+  }
+}
+
+Object.assign(ClientEngine.prototype, EventSourceMixin);
+
+export default ClientEngine;
